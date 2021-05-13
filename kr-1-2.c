c00856480c1356eddd527cad9620c104955dd0dd
Katherine <talk2keling@gmail.com>
2021-05-13 14:02:08 -0400

    add exercise 1-4

--- kr-1-2.c
+++ kr-1-2.c
@@ -38,4 +38,21 @@ void main()
         fahr = fahr + step;
     }
 
+    /* Exercise 1-4: print the corresponding Celsius to Fahrenheit Table */
+    // float fahr, celsius;
+    // int lower, upper, step;
+
+    // lower = 0;      /* lower limit of temperature table */
+    // upper = 300;    /* upper limit */
+    // step = 20;      /* step size */
+
+    // celsius = lower;
+
+    // printf("celsius fahr\n");
+    // while (celsius <= upper) {
+    //     fahr = (9.0 / 5.0) * celsius + 32.0;
+    //     printf("%3.0f%8.0f\n", celsius, fahr); 
+    //     celsius = celsius + step;
+    // }
+
 }
