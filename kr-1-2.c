b62c5aaecd3933dd73f79904d4e4773265435c94
Katherine <talk2keling@gmail.com>
2021-05-13 13:57:57 -0400

    add 1-2

---
+++ kr-1-2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void main()
+{
+    /* 1.2 Variables and Arithmetic Expressions */
+    /* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 */
+
+    /* old ver. (not accurate due to integer arithmetic */
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr- 32) / 9;
+        // printf("%d\t%d\n", fahr, celsius);
+        printf("%3d\t%6d\n", fahr, celsius); /* added fields so numbers are right justified*/
+        fahr = fahr + step;
+    }
+
+    /* new version (uses floating-point arithmetic for a more accurate conversion*/
+    // float fahr, celsius;
+    // int lower, upper, step;
+
+    // lower = 0;      /* lower limit of temperature table */
+    // upper = 300;    /* upper limit */
+    // step = 20;      /* step size */
+
+    // fahr = lower;
+
+    // printf("fahr celsius\n");
+    // while (fahr <= upper) {
+    //     celsius = (5.0 / 9.0) * (fahr- 32.0);
+    //     printf("%3.0f%6.1f\n", fahr, celsius); 
+    //     fahr = fahr + step;
+    // }
+
+}
