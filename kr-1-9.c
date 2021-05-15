62696a7c2be2254230dbb52d523a03353637cdb5
Katherine <talk2keling@gmail.com>
2021-05-15 00:49:46 -0400

    add 1-9

--- kr-1-9.c
+++ kr-1-9.c
@@ -5,11 +5,14 @@
 int getline1(char line[], int maxline);
 void copy(char to[], char from[]);
 int printlongestline();
+int revprintlongestline();
+int printlinesover80();
 
 main()
 {
     // printlongestline();
-    
+    // revprintlongestline();
+    printlinesover80();
 }
 
 /* print longest input line */
@@ -55,3 +58,41 @@ void copy(char to[], char from[])
     while ((to[i] = from[i]) != '\0')
         ++i;
 }
+
+/* Exercise 1-16: Revise the main routine of the longest-line program so it will correctly print the length of 
+an arbitrarily long input lines, and as much as possible of the text */
+int revprintlongestline()
+{
+    int len;
+    int max;
+    int numlen;
+    char line[MAXLINE];
+    char longest[MAXLINE];
+    
+    max = 0;
+    while ((len = getline1(line, MAXLINE)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+            numlen = len;
+        }
+    if (max > 0)
+        printf("length of line: %d ; longest string: %s", numlen, longest);
+    return 0;
+}
+
+/* Exercise 1-17: Write a program to print all input lines that are longer than 80 characters. */
+int printlinesover80()
+{
+    int len;
+    int max;
+    char line[MAXLINE];
+    char longest[MAXLINE];
+
+    while ((len = getline1(line, MAXLINE)) > 0) 
+        if ((len - 1) > 80) { //getline1 adds 1 from \n
+            printf("length: %d ; string: %s", (len - 1), line);
+        }
+    return 0;
+}
+
