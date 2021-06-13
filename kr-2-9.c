f541fddb01935519e031f0a0bf01deddaa1c1d01
kathling <talk2keling@gmail.com>
2021-06-13 00:19:04 -0400

    added kr-3-4

---
+++ kr-2-9.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // 1 : 0001
+    // 2 : 0010
+    // 3 : 0011
+    // 5 : 0101
+    // int n;
+    // printf("size of n: %d\n", sizeof n);
+
+    // n = 1;
+    // printf("size of n: %d\n", sizeof n);
+
+    // n = n & 2;
+    // printf("size of n: %d\n", sizeof n);
+    // printf("%d\n", n);
+
+    // printf("=========================\n");
+    // n = 1;
+    // n = n | 3;
+    // printf("size of n: %d\n", sizeof n);
+    // printf("%d\n", n);
+    
+    int n = 3;
+    printf("You have %d item%s.\n", n, n == 1 ? "" : "s");
+
+}
